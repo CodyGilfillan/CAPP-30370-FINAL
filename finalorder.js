560d6d5f660a6bbdfed5a19d4c4535eea296fd61
CodyGilfillan <cgilfill@nd.edu>
2013-12-14 03:49:51 -0500

    Final Website
    1st draft

---
+++ finalorder.js
@@ -0,0 +1,11 @@
+	document.getElementByClass('img').addEventListener("click", function(){
+	var box = document.getElementById('sb')
+  			 console.log(box.getAttribute('src'));
+  			 console.log(box.getAttribute('alt'));
+  			});
+  			
+ document.getElementById('my_button').addEventListener("click", function(){
+			var button = alert('see ya then :)')});
+			
+			
+			 			
---
+++ seeyathen.js
@@ -0,0 +1,2 @@
+document.getElementById('my_button').addEventListener("click", function(){
+			var button = alert('see ya then :)')});
