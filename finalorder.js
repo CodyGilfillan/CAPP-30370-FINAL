ba6865fd2588116443cd88491f0adb13e07f3d6c
CodyGilfillan <cgilfill@nd.edu>
2013-12-16 16:47:36 -0500

    Final Submission #2

--- finalorder.js
+++ finalorder.js
@@ -3,21 +3,21 @@ $('.section').on("click", function afterPic(){
 	console.log('alt: ' + this.alt);
 	console.log('width: ' + this.width);
 	console.log('height: ' + this.height);
-    });
+});
     
 $('h1, h2, h3, h4, h5, h6').on("click", function(){
 	console.log('text: ' + $( this ).text());
 	console.log('color: ' + $( this ).css( 'color' ));
 	console.log('font-size: ' +  $( this ).css( 'fontSize' ));
-    });     
+});     
     
     
 $(window).load(function() {
 	var $windowWidth = $(window).width();
 	console.log('browser width: ' + $windowWidth);   
-	});  
+});  
 	
 $(window).resize(function() {
 	var $windowWidth = $(window).width();
 	console.log('browser width: ' + $windowWidth);   
-	});  	
+});  	
--- finalproject.js
+++ finalproject.js
@@ -1,15 +1,15 @@
 $(window).load(function() {
 	var $windowWidth = $(window).width();
 	console.log('browser width: ' + $windowWidth);   
-	});  
+});  
 	
 $(window).resize(function() {
 	var $windowWidth = $(window).width();
 	console.log('browser width: ' + $windowWidth);   
-	});  	
+});  	
 	
 $('h1, h2, h3, h4, h5, h6').on("click", function(){
 	console.log('text: ' + $( this ).text());
 	console.log('color: ' + $( this ).css( 'color' ));
 	console.log('font-size: ' +  $( this ).css( 'fontSize' ));
-    });  	
+});  	
